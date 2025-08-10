ef0fee5c78c0270a4278f642225aa60e4a57106e
nikcich <nrc170001@utdallas.edu>
2025-08-09 19:27:20 -0700

    Beef up testing

--- tests/test_csv_parsing.py
+++ tests/test_csv_parsing.py
@@ -1,6 +1,7 @@
 import unittest
 from utils.parse_csv import parse_csv_to_transactions
 from utils.csv_definitions import amex_csv_definition, wf_activity_csv_definition, wf_csv_definition
+from utils.csv_validator import validate_csv
 
 expected_transactions = [
     {'date': '02/15/2023', 'description': 'Groceries', 'amount': 45.67},
@@ -11,7 +12,7 @@ expected_transactions = [
 
 class TestParseCSVToTransactions(unittest.TestCase):
     def test_amex_csv_definition_parse(self):
-        file_path = './tests/mocks/good/amex_activity_example.csv'
+        file_path = './tests/mocks/good/amex_activity_good_example.csv'
 
         try:
             transactions = parse_csv_to_transactions(file_path, amex_csv_definition)
@@ -28,8 +29,22 @@ class TestParseCSVToTransactions(unittest.TestCase):
             self.assertEqual(et['description'], t.description)
             self.assertEqual(et['amount'], t.amount)
 
+    def test_amex_csv_definition_validation_fail(self):
+        file_paths = [
+            './tests/mocks/bad/amex_activity_bad_example_1.csv',
+            './tests/mocks/bad/amex_activity_bad_example_2.csv'
+        ]
+
+        for file_path in file_paths:
+            result = validate_csv(file_path, amex_csv_definition)
+            self.assertEqual(result, False)
+
+    #################################################################################
+    #################################################################################
+    #################################################################################
+
     def test_wf_activity_csv_definition_parse(self):
-        file_path = './tests/mocks/good/wf_activity_example.csv'
+        file_path = './tests/mocks/good/wf_activity_good_example.csv'
 
         try:
             transactions = parse_csv_to_transactions(file_path, wf_activity_csv_definition)
@@ -46,8 +61,22 @@ class TestParseCSVToTransactions(unittest.TestCase):
             self.assertEqual(et['description'], t.description)
             self.assertEqual(et['amount'], t.amount)
 
+    def test_wf_activity_csv_definition_validation_fail(self):
+        file_paths = [
+            './tests/mocks/bad/wf_activity_bad_example_1.csv',
+            './tests/mocks/bad/wf_activity_bad_example_2.csv'
+        ]
+
+        for file_path in file_paths:
+            result = validate_csv(file_path, wf_activity_csv_definition)
+            self.assertEqual(result, False)
+
+    #################################################################################
+    #################################################################################
+    #################################################################################
+
     def test_wf_spending_report_csv_definition_parse(self):
-        file_path = './tests/mocks/good/wf_spending_report_example.csv'
+        file_path = './tests/mocks/good/wf_spending_report_good_example.csv'
 
         try:
             transactions = parse_csv_to_transactions(file_path, wf_csv_definition)
@@ -63,3 +92,17 @@ class TestParseCSVToTransactions(unittest.TestCase):
             self.assertEqual(et['date'], t.date)
             self.assertEqual(et['description'], t.description)
             self.assertEqual(et['amount'], t.amount)
+
+    def test_wf_activity_csv_definition_validation_fail(self):
+        file_paths = [
+            './tests/mocks/bad/wf_spending_report_bad_example_1.csv',
+            './tests/mocks/bad/wf_spending_report_bad_example_2.csv'
+        ]
+
+        for file_path in file_paths:
+            result = validate_csv(file_path, wf_csv_definition)
+            self.assertEqual(result, False)
+
+    #################################################################################
+    #################################################################################
+    #################################################################################
